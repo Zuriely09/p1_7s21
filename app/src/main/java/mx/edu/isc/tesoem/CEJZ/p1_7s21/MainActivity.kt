e2c42988d3f3c6383428cfd23d803a56a75470b4
jenniferzuriely <jenniferzurienriquez09@gmail.com>
2023-09-30 20:14:44 -0600

    p1

---
+++ app/src/main/java/mx/edu/isc/tesoem/CEJZ/p1_7s21/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.edu.isc.tesoem.CEJZ.p1_7s21
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
